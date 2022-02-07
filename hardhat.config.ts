859fe10de0b5864a806d44cc78a8b7be83b6ccbf
Emanuel Tesar <e.tesarr@gmail.com>
2022-02-07 14:43:05 +0100

    Minor README change, remove unnecessary hardhat config part

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,6 @@ import { HardhatUserConfig } from 'hardhat/types';
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'localhost',
-  solidity: {
-    compilers: [{ version: '0.8.9', settings: {} }],
-  },
   networks: {
     localhost: {
       url: 'http://127.0.0.1:8545/',
