192345f8cf168634f7237808e300368ec6822cbd
aquarat <blue@aquarat.co.za>
2022-02-12 16:26:08 +0200

    Add beacon filename sanitiser function

--- src/utils.ts
+++ src/utils.ts
@@ -5,6 +5,21 @@ export type GoResult<T, E = Error> = GoResultSuccess<T> | GoResultError<E>;
 export const GO_ERROR_INDEX = 0;
 export const GO_RESULT_INDEX = 1;
 
+export const sanitiseFilename = (filename: string) => {
+  const illegalRe = /[\/?<>\\:*|"]/g;
+  // eslint-disable-next-line no-control-regex
+  const controlRe = /[\x00-\x1f\x80-\x9f]/g;
+  const reservedRe = /^\.+$/;
+  const windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i;
+
+  return filename
+    .replace(illegalRe, '_')
+    .replace(controlRe, '_')
+    .replace(reservedRe, '_')
+    .replace(windowsReservedRe, '_')
+    .toLocaleLowerCase();
+};
+
 export const successFn = <T>(value: T): GoResultSuccess<T> => {
   const result: any = [null, value];
   // eslint-disable-next-line functional/immutable-data
